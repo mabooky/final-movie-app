3406a41b773f4c3c1a4f2b83ca1f30f71df2eac6
Heungrok Park <mabookyggl@gmail.com>
2026-06-16 14:20:17 +0900

    Week 14: 404 예외 처리 및 vercel.json 세팅 완료

--- src/router/index.js
+++ src/router/index.js
@@ -2,26 +2,33 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import MoviesView from '../views/MoviesView.vue';
 import MovieDetailView from '@/views/MovieDetailView.vue';
+import NotFoundView from '@/views/NotFoundView.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/', // 기본 주소 (localhost:5173/)
-      name: 'home',
-      component: HomeView // 이 주소로 오면 HomeView 컴포넌트를 띄워라!
-    },
-    {
-      path: '/movies', // (localhost:5173/movies)
-      name: 'movies',
-      component: MoviesView // 이 주소로 오면 MoviesView 컴포넌트를 띄워라!
-    },
-    {
-      path: '/movies/:id', // (localhost:5173/movies/12345)
-      name: 'movie-detail',
-      component: MovieDetailView // 이 주소로 오면 MovieDetailView 컴포넌트를 띄워라!
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/', // 기본 주소 (localhost:5173/)
+            name: 'home',
+            component: HomeView // 이 주소로 오면 HomeView 컴포넌트를 띄워라!
+        },
+        {
+            path: '/movies', // (localhost:5173/movies)
+            name: 'movies',
+            component: MoviesView // 이 주소로 오면 MoviesView 컴포넌트를 띄워라!
+        },
+        {
+            path: '/movies/:id', // (localhost:5173/movies/12345)
+            name: 'movie-detail',
+            component: MovieDetailView // 이 주소로 오면 MovieDetailView 컴포넌트를 띄워라!
+        },
+        // [추가] 정해진 주소가 아닌 모든 경로를 404 페이지로 연결!
+        {
+            path: '/:pathMatch(.*)*',
+            name: 'not-found',
+            component: NotFoundView
+        }
+    ]
 });
 
 export default router;
